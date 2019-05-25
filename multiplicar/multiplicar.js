7168775aae5695ba15e4a9dc4e15787e8b04a07f
zamora4282 <zamora4282@gmail.com>
2019-05-25 20:47:14 +0200

    Refactorizamo el código

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+let crearArchivo = ( base )=>{
+ 
+    return new Promise((resolve,reject) => {
+        let data = '';
+
+        if( !Number( base ) ){
+            reject(`El valor introducido ${ base } No es un número`);
+            return;
+        }
+        for(let i=1;i<11;i++){
+            console.log(base*i);
+            data += `${base}*${i} = ${base*1}\n`;
+        }
+
+
+        fs.writeFile(`archivos/tabla-${base}.txt`, data, (err) => {
+        if (err) reject (err)
+        else    
+            reject(`tabla-${base}.txt`);
+        
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo
+}
+ 
+
