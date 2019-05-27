876d93b045895f2d31797a004a96c11361c763a4
zamora4282 <zamora4282@gmail.com>
2019-05-27 21:27:56 +0200

    pasar parametros por terminal

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const { crearArchivo } = require('./multiplicar/multiplicar');
 
 let base = 7;
 
-crearArchivo(base)
+
+let arv = process.argv[2];
+
+let parametros = arv.split('=');
+crearArchivo(parametros[1])
     .then(archivo => console.log(`Archivo creado ${ archivo }`))
     .catch(e => console.log(e));
