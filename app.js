926112ae794f1c976618605e371bb83fe5e1f864
zamora4282 <zamora4282@gmail.com>
2019-06-10 17:18:50 +0200

    aplicando yargs

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+const argv = require ('yargs')
+                    .command('Listar', 'Imprime en consola la tabla del número indicado',{
+                        base : {
+                            demand: true,
+                            alias: 'b'
+                        },
+                        limite: {
+                            alias: 'l',
+                            default: 10
+                        }
+                    })
+                    .help()
+                    .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
 let base = 7;
+console.log('limite',argv.limite);
 
 
-let arv = process.argv[2];
-
-let parametros = arv.split('=');
-crearArchivo(parametros[1])
-    .then(archivo => console.log(`Archivo creado ${ archivo }`))
-    .catch(e => console.log(e));
+// let parametros = arv.split('=');
+// crearArchivo(parametros[1])
+//     .then(archivo => console.log(`Archivo creado ${ archivo }`))
+//     .catch(e => console.log(e));
